38a824d771e928261ce6505b093f7118779b133e
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-08-05 00:49:03 +0500

    10dars

---
+++ js/index.js
@@ -0,0 +1,235 @@
+// String ===
+
+// 1 misol
+
+// let str = ('salom bolalar yaxshmisilar');
+// function sum(arg) {
+//   let arr = arg.split('a');
+//   return arr.length -1;
+  
+// }
+
+// console.log(sum(str));
+ 
+
+// 2 misol
+
+// let res = ('salom');
+// function name(arg) {
+  
+//   return arg.slice(-3);
+// }
+// console.log(name(res));
+
+// 3 misol
+
+// let str = 'salom xayr';
+
+// function sum(arg) {
+//   let res = arg.split(' ');
+//   let res1 = res.map(function(value) {
+//     return value.charAt(0).toUpperCase() + value.slice(1);
+//   });
+//   return res1.join(' ');
+// }
+
+// console.log(sum(str));
+
+// 4 misol
+// function sum(str) {
+//   return str.split(' ').join('');
+// }
+
+// // Misollar
+// console.log(sum('salom   xayr                                                                                   tung'));
+
+
+// 5 misol
+// let res = [1, 2, 3, 4, 5, 6];
+// function sum(arr) {
+//   return arr.slice().reverse();
+// }
+
+// console.log(sum(res));
+
+// 6 misol
+
+// let res = ('salom  bollar');
+// function name(arg) {
+//   return arg.length;
+  
+// }
+// console.log(name(res));
+
+
+// 7 misol
+
+// let res = ('salom ');
+// function name(arg) {
+//   return res.repeat (3)
+  
+// }
+// console.log(name(res));
+
+
+// 8 misol
+// let res = ['salom', 1, 23, 4, 'xayr'];
+// function sum(arr) {
+  
+//   return arr.filter(item => typeof item === 'number');
+// }
+
+// console.log(sum(res)); 
+
+
+// 9 misol
+
+// let res = ('salom xayr');
+// function name(arg) {
+//   let arr = arg.split (' ')
+//   return arr [0];
+  
+// }
+// console.log(name(res));
+
+// 10 misol
+// let res = 'JavaScript  js';
+// function sum(str) {
+//   return str.toLowerCase().includes('js');
+// }
+
+// console.log(sum(res)); 
+
+
+
+// Massivlar
+
+
+
+// 1 misol
+
+// function findMax(arr) {
+//   return Math.max(...arr);
+// }
+// const res = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(findMax(res)); 
+
+
+// 2 Misol
+
+// let res = [1,2,3,4,5,67,8,]
+// let arr = [];
+// function sum(arg) {
+//   for (let i = 0; i < arg.length; i++) {
+//     if(arg[i] % 2 == 0){
+//       arr.push(arg[i])
+//     }
+    
+//   }
+//   return arr
+// }
+// console.log(sum(res));
+
+// 3 Misol
+
+// let res = [1,2,3]
+// let arr = 0;
+// function sum(arg) {
+//   for (let i = 0; i < arg.length; i++) {
+//    arr = arr + arg[i]
+    
+//   }
+//   return arr
+// }
+// console.log(sum(res));
+
+
+// 4 Misol
+// let res = [1,2,3]
+// let arr = [];
+// function sum(arg) {
+//   for (let i = 0; i < arg.length; i++) {
+
+//   arr.push(arg[i] * 2)
+    
+//   }
+//   return arr
+// }
+// console.log(sum(res));
+
+
+// 5 Misol
+// let res = [1,2,3,-7,-4,-5]
+// let arr = [];
+// function sum(arg) {
+//   for (let i = 0; i < arg.length; i++) {
+//     if(arg[i] > 0)
+
+//   arr.push(arg[i])
+    
+//   }
+//   return arr
+// }
+// console.log(sum(res));
+
+
+// 6 Misol
+
+// 7 Misol
+
+// let res = [1,2,3]
+// let res1 = [4,5,6]
+// function sum(arg,arg1) {
+//   return arg.concat(arg1) 
+// }
+// console.log(sum(res,res1));
+
+
+// 8 Misol
+
+// let res = [1,2,3,4,5,6,7]
+// function sum(arg) {
+//   return arg.reverse();
+// }
+// console.log(sum(res));
+
+// 9 Misol
+
+
+// let res = [1,2,3,4,5,67,8,]
+// let arr = [];
+// function sum(arg) {
+//   for (let i = 0; i < arg.length; i++) {
+//     if(arg[i] % 2 == 1){
+//       arr.push(arg[i])
+//     }
+    
+//   }
+//   return arr
+// }
+// console.log(sum(res));
+
+
+// 10 Misol
+
+// let res = [1,2,3,4,5,67,8,]
+
+// function sum(arg) {
+//   }
+//   return arr
+// }
+// console.log(sum(res));
+
+
+// Obyekt 
+
+
+// 1 Misol
+
+const people = [
+  { name: "Ali", age: 25 },
+  { name: "Vali", age: 30 },
+  { name: "Hasan", age: 28 }
+];
+
+
